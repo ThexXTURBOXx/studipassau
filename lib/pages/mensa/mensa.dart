e6d4adaeccaa5d4eb399a5252b850bef05efb36b
Nico Mexis <nico.mexis@kabelmail.de>
2022-04-22 14:43:14 +0200

    Remove artifact from copy pasting stuff

--- lib/pages/mensa/mensa.dart
+++ lib/pages/mensa/mensa.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart' hide Interval;
+import 'package:flutter/material.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:openmensa/openmensa.dart';
 import 'package:sprintf/sprintf.dart';
