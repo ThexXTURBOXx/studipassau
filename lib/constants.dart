c1ad011869a38d34ee68d2a0edfac60e26f2425b
Nico Mexis <nico.mexis@kabelmail.de>
2022-05-18 23:19:03 +0200

    Add better debug printing method

--- lib/constants.dart
+++ lib/constants.dart
@@ -93,3 +93,9 @@ Color getColor(int index) =>
 
 String get appVersion => '${packageInfo.version} '
     '(${packageInfo.buildNumber})';
+
+/// Debug method as described in https://github.com/flutter/flutter/issues/22665
+void printWrapped(String text) {
+  final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
+  pattern.allMatches(text).forEach((match) => debugPrint(match.group(0)));
+}
