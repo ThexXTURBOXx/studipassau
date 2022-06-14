f2800b41af79b5679a47215e1c0adf65914af652
Nico Mexis <nico.mexis@kabelmail.de>
2022-06-14 23:49:47 +0200

    Obfuscate env values
    Closes #18

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -2,11 +2,11 @@ import 'package:envify/envify.dart';
 
 part 'env.g.dart';
 
-@Envify(path: '.env')
+@Envify(path: '.env', obfuscate: true)
 abstract class Env {
-  static const consumerKey = _Env.consumerKey;
+  static final String consumerKey = _Env.consumerKey;
 
-  static const consumerSecret = _Env.consumerSecret;
+  static final String consumerSecret = _Env.consumerSecret;
 
-  static const sentryDsn = _Env.sentryDsn;
+  static final String sentryDsn = _Env.sentryDsn;
 }
