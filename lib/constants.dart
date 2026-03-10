3949401db5442831d4d312767e1bcb3c03de4c72
Nico Mexis <nico.mexis@kabelmail.de>
2026-03-10 20:29:20 +0100

    Make printWrapped more applicable

--- lib/constants.dart
+++ lib/constants.dart
@@ -230,7 +230,9 @@ Future<void> installRootCertificates() async {
 }
 
 /// Debug method as described in https://github.com/flutter/flutter/issues/22665
-void printWrapped(String text) {
+void printWrapped(dynamic text) {
   final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
-  pattern.allMatches(text).forEach((match) => debugPrint(match.group(0)));
+  pattern
+      .allMatches(text.toString())
+      .forEach((match) => debugPrint(match.group(0)));
 }
